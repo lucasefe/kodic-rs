ccd984f133892f2f5cb90c611d4f402bed913dd8
Lucas Florio <lucasefe@gmail.com>
2015-09-26 23:46:23 -0700

    Get Version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+extern crate hyper;
+extern crate rustc_serialize;
+
 use std::env;
 use std::process;
+use std::io::Read;
+use rustc_serialize::json;
+use hyper::header::{Headers, Authorization, Basic, ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+use hyper::Client;
 
 static VALID_COMMANDS: &'static [ &'static str ] = &["version"];
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct RequestBody {
+  jsonrpc: String,
+  method: String,
+  id: u8
+}
 
 // TODO: Check for proper error code to exit with. 
 fn exit_with_error(message: &str) {
@@ -15,7 +30,36 @@ fn run(command: &str) {
     }
     
     println!("Executing command: {}", command);
+    
+    let body = RequestBody {
+        jsonrpc: "2.0".to_string(), 
+        method:  "JSONRPC.Version".to_string(),
+        id: 1
+    };
 
+    let client = Client::new();
+    let mut headers = Headers::new();
+    headers.set(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)])));
+    headers.set(
+        Authorization(
+            Basic { 
+                username: "xbmc".to_owned(), 
+                password: Some("xbmc".to_owned())
+            }
+        )
+    );
+    
+    let json_body = json::encode(&body).unwrap();
+    let mut res = client.post("http://10.0.1.3:8080/jsonrpc")
+        .body(&json_body)
+        .headers(headers)
+        .send()
+        .unwrap();
+
+    let mut buffer = String::new();
+    res.read_to_string(&mut buffer).unwrap();
+    println!("{}", buffer);
+    println!("{}", res.status_raw().0);
 }
 
 // parse action
